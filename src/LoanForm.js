ee9b2b25114d3770d60bcecb603d30621ed6fb5a
Samar AYADI <ayadisamardev@gmail.com>
2023-08-03 14:01:06 +0100

    Building up the logic

--- src/LoanForm.js
+++ src/LoanForm.js
@@ -1,37 +1,93 @@
 import "./FormStyles.css";
 import Modal from "./Modal";
+
+import { useState } from "react";
 function LoanForm() {
+  const [loanInputs, setLoanInputs] = useState({
+    name: "",
+    phoneNumber: "",
+    age: "",
+    isEmployee: false,
+    salaryRange: "",
+  });
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    alert("hello");
+  }
+
+  const btnIsDisabled =
+    loanInputs.name == "" ||
+    loanInputs.age == "" ||
+    loanInputs.phoneNumber == "";
+   
+  // let btnClasses = ""
+  // if (btnIsDisabled) {
+  //   btnClasses = "disabled"
+  // } else {
+  //   btnClasses= ""
+  // }
   return (
-    <div
-      className="flex"
-      style={{ flexDirection: "column" }}
-    >
+    <div className="flex" style={{ flexDirection: "column" }}>
       <form id="loan-form" className="flex" style={{ flexDirection: "column" }}>
         <h1>Requesting a Loan</h1>
         <hr />
         <label>Name :</label>
-        <input />
+        <input
+          value={loanInputs.name}
+          onChange={(event) => {
+            setLoanInputs({ ...loanInputs, name: event.target.value });
+          }}
+        />
 
         <label>Phone Number :</label>
-        <input />
+        <input
+          value={loanInputs.phoneNumber}
+          onChange={(event) => {
+            setLoanInputs({ ...loanInputs, phoneNumber: event.target.value });
+          }}
+        />
 
         <label>Age :</label>
-        <input />
+        <input
+          value={loanInputs.age}
+          onChange={(event) => {
+            setLoanInputs({ ...loanInputs, age: event.target.value });
+          }}
+        />
 
-        <label style={{marginTop: "30px"}}>Are you an employee ? </label>
-        <input id="checkbox" type="checkbox" />
+        <label style={{ marginTop: "30px" }}>Are you an employee ? </label>
+        <input
+          type="checkbox"
+          checked={loanInputs.isEmployee}
+          onChange={(event) => {
+            setLoanInputs({ ...loanInputs, isEmployee: event.target.checked });
+          }}
+        />
 
         <label>Salary</label>
-        <select>
+        <select
+          value={loanInputs.salaryRange}
+          onChange={(event) => {
+            setLoanInputs({ ...loanInputs, salaryRange: event.target.value });
+          }}
+        >
           <option>less than 500$</option>
           <option>between 500$ and 2000</option>
           <option>above 2000</option>
         </select>
 
-        <button id="submit-loan-btn">Submit</button>
-          </form>
-          
-          <Modal />
+        <button
+          className={btnIsDisabled ? "disabled" : "" }
+          onClick={handleFormSubmit}
+          disabled={btnIsDisabled}
+          id="submit-loan-btn"
+        >
+          Submit
+        </button>
+      </form>
+
+      {/* <Modal /> */}
     </div>
   );
 }
